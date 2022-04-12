35172ac35c5095bda76c941e3796749ad3f80e97
Matthew Ng <Matthew.Ng.1@stonybrook.edu>
2022-04-12 18:46:45 -0400

    Removing some useless files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # banana-vision
 Stonybrook ESE 440 Senior Design Project
+
+ObjectDet_w_Depth.py was used to demo our WIP in December 2021
--- tts.py
+++ tts.py
@@ -1,23 +1,12 @@
-import pyttsx3
+from gtts import gTTS 
+import os
 import time
 
-def tts_msg(msg, rate, volume) : 
-    try:
-        engine = pyttsx3.init() # object creation 
-    except:
-        print("Engine Init Failed")
-    engine.setProperty('rate', rate)     # setting up new voice rate
-    engine.setProperty('volume', volume)    # setting up volume level between 0 and 1
-    voices = engine.getProperty('voices')       #getting details of current voice
-    engine.setProperty('voice', voices[0].id)   #changing index, changes voices. 1 for female
-    engine.say(msg)
-    print("INSIDE TTS FUNCTION")
-    engine.runAndWait()
-    #engine.stop()
-    #engine.save_to_file('Chair Right 1 meter', 'test.mp3')
-    #engine.runAndWait()
-    
-text = "Banana Engineer Moment"
+def tts_msg(msg, language) :   
+    speech = gTTS(text = msg, lang = language, slow = False)
+    speech.save("text.mp3")
+    os.system("start text.mp3")
+
 start_time = time.time()
-tts_msg(text, 130, 1.0)
+tts_msg("horseleft1meter", 'en')
 print("--- %s seconds ---" % (time.time() - start_time))
