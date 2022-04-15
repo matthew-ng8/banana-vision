c81ae6db866a400935d1d729a8b88fefec3a4b32
Matthew Ng <Matthew.Ng.1@stonybrook.edu>
2022-04-15 12:54:40 -0400

    Threading and multirpocessing code in place. Threading code commeted out removed some useles imports and lena png.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from tts import tts_msg
+import tts
 import pyttsx3
 import time
-engine = pyttsx3.init() # object creation 
-
-from ast import List
-from pickle import NONE
+# import queue
+from multiprocessing import Queue
+# engine = pyttsx3.init() # object creation 
 import cv2
 import pyrealsense2 as rs
 import numpy as np
@@ -14,150 +13,153 @@ from surface import testSurface
 from sections import getSection3
 from frame_data import frame_data
 
+if __name__ == '__main__':
+    objectDictionary = {} #dictionary to hold objects
+    creationTime= time.time()
+    lifeSpan = 180
+    MAX_FRAMES = 5
+    DEPTH_TOLERANCE = .20
+    CENTER_TOLERANCE = 3
 
-#dictionary to hold objects
-objectDictionary = {}
-creationTime= time.time()
-lifeSpan = 180
-MAX_FRAMES = 5
-DEPTH_TOLERANCE = .20
-CENTER_TOLERANCE = 3
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+    test = 0
+    # Get device product line for setting a supporting resolution
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
 
-# Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
-align_to = rs.stream.color
-align = rs.align(align_to)
-test = 0
-# Get device product line for setting a supporting resolution
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
-device_product_line = str(device.get_info(rs.camera_info.product_line))
+    found_rgb = False
+    for s in device.sensors:
+        if s.get_info(rs.camera_info.name) == 'RGB Camera':
+            found_rgb = True
+            break
+    if not found_rgb:
+        print("The demo requires Depth camera with Color sensor")
+        exit(0)
 
-found_rgb = False
-for s in device.sensors:
-    if s.get_info(rs.camera_info.name) == 'RGB Camera':
-        found_rgb = True
-        break
-if not found_rgb:
-    print("The demo requires Depth camera with Color sensor")
-    exit(0)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    if device_product_line == 'L500':
+        config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+    else:
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-if device_product_line == 'L500':
-    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-else:
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    # Start streaming
+    pipeline.start(config)
+    thresh = 0.65 # Threshold to detect object
+    classNames= []
+    classFile = 'coco.names'
+    with open(classFile,'rt') as f:
+        classNames = f.read().rstrip('\n').split('\n')
 
-# Start streaming
-pipeline.start(config)
-thres = 0.65
+    configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+    weightsPath = 'frozen_inference_graph.pb'
+    net = cv2.dnn_DetectionModel(weightsPath,configPath)
+    net.setInputSize(320,320)
+    net.setInputScale(1.0/ 127.5)
+    net.setInputMean((127.5, 127.5, 127.5))
+    net.setInputSwapRB(True)
+    count = 0
+    # q = queue.Queue()
+    q = Queue()
+    # tts_thread = tts.TTSThread(q)
+    tts_thread = tts.TTSProcess(q)
+    # tts_thread.start()
+    try:
+        while True:
+            time1 = time.time_ns()
+            
+            # if count%5 ==0:
+            #     print(time1)
+            count = count+1
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            aligned_frames = align.process(frames)
+            depth_frame = aligned_frames.get_depth_frame()
+            color_frame = aligned_frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
 
- # Threshold to detect object
-classNames= []
-classFile = 'coco.names'
-with open(classFile,'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
 
-configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = 'frozen_inference_graph.pb'
-net = cv2.dnn_DetectionModel(weightsPath,configPath)
-net.setInputSize(320,320)
-net.setInputScale(1.0/ 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
-count = 0
-try:
-    while True:
-        time1 = time.time_ns()
-        
-        # if count%5 ==0:
-        #     print(time1)
-        count = count+1
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        aligned_frames = align.process(frames)
-        depth_frame = aligned_frames.get_depth_frame()
-        color_frame = aligned_frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
+            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            
 
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+            depth_colormap_dim = depth_colormap.shape
+            color_colormap_dim = color_image.shape
+            
+            # Convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-        
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha = 0.03), cv2.COLORMAP_JET)
+            img = color_image
+            classIds, confs, bbox = net.detect(img,confThreshold=thresh)
 
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
-        
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha = 0.03), cv2.COLORMAP_JET)
-        img = color_image
-        classIds, confs, bbox = net.detect(img,confThreshold=thres)
-
-        if len(classIds) != 0:
-            for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-                cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-                cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
-                cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-                cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                x = (int) ((box[2] + box[0] + (box[0])) /2)
-                y = (int) ((box[3] + box[1] + (box[1])) /2)
-                cv2.rectangle(img,(x-1, y-1, 2, 2 ),color=(0,255,0),thickness=2)
-                depth = depth_frame.get_distance(x,y)
-                if (lifeSpan <= (time.time() - creationTime)):
-                    objectDictionary.clear()
-                    creationTime = time.time()
-                if (objectDictionary.get(classNames[classId-1]) == None):
-                    objectDictionary.update({classNames[classId-1]: [] })
-                    dataList = objectDictionary.get(classNames[classId-1])
-                    dataList.append(frame_data((x,y), DEPTH_TOLERANCE, MAX_FRAMES))
-                else:
-                    dataList = objectDictionary.get(classNames[classId-1])
-                    isAdded = False
-                    for index in dataList:
-                        if (index.similarCenter((x,y))):
-                            if (index.addFrame(depth)): #addFrame returns true if at max frames
-                                if (index.checkDepths() and index.withinDepth(2)):  #check depth and changed to 1
-                                    xDir = getSection3(x)
-                                    stringTTS = (classNames[classId-1] + " in the " + " " + xDir)
-                                    #stringTTS = (classNames[classId-1] + " in the " + " " + xDir + "\nCenter Coordinates: (" + str(x) + "," + str(y) + ")")
-                                    print(stringTTS)
-                                    threading.Thread(tts_msg(stringTTS, 280, 100.0))
-                                dataList.remove(index)    
-                            else:
-                                index.setCenter((x,y))
-                                isAdded = True
-                            break
-                    if (not isAdded):
+            if len(classIds) != 0:
+                for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    x = (int) ((box[2] + box[0] + (box[0])) /2)
+                    y = (int) ((box[3] + box[1] + (box[1])) /2)
+                    cv2.rectangle(img,(x-1, y-1, 2, 2 ),color=(0,255,0),thickness=2)
+                    depth = depth_frame.get_distance(x,y)
+                    if (lifeSpan <= (time.time() - creationTime)):
+                        objectDictionary.clear()
+                        creationTime = time.time()
+                    if (objectDictionary.get(classNames[classId-1]) == None):
+                        objectDictionary.update({classNames[classId-1]: [] })
+                        dataList = objectDictionary.get(classNames[classId-1])
                         dataList.append(frame_data((x,y), DEPTH_TOLERANCE, MAX_FRAMES))
-                            
-        else: 
-            surfacetext = testSurface(depth_frame)
-            if(surfacetext != "NONE") :
-                threading.Thread(tts_msg(surfacetext, 280, 100.0))
+                    else:
+                        dataList = objectDictionary.get(classNames[classId-1])
+                        isAdded = False
+                        for index in dataList:
+                            if (index.similarCenter((x,y))):
+                                if (index.addFrame(depth)): #addFrame returns true if at max frames
+                                    if (index.checkDepths() and index.withinDepth(2)):  #check depth and changed to 1
+                                        xDir = getSection3(x)
+                                        stringTTS = (classNames[classId-1] + " in the " + " " + xDir)
+                                        #stringTTS = (classNames[classId-1] + " in the " + " " + xDir + "\nCenter Coordinates: (" + str(x) + "," + str(y) + ")")
+                                        print(stringTTS)
+                                        q.put(stringTTS)
+                                        # threading.Thread(tts_msg(stringTTS, 280, 100.0))
+                                    dataList.remove(index)    
+                                else:
+                                    index.setCenter((x,y))
+                                    isAdded = True
+                                break
+                        if (not isAdded):
+                            dataList.append(frame_data((x,y), DEPTH_TOLERANCE, MAX_FRAMES))
+                                
+            # else: 
+            #     surfacetext = testSurface(depth_frame)
+            #     if(surfacetext != "NONE") :
+            #         threading.Thread(tts_msg(surfacetext, 280, 100.0))
 
-        # if test < 2:
-        #     display9x9(depth_colormap_dim,color_image, depth_frame)
-        #     test = test + 1
-        
-        images = np.hstack((color_image, depth_colormap))
+            # if test < 2:
+            #     display9x9(depth_colormap_dim,color_image, depth_frame)
+            #     test = test + 1
+            
+            images = np.hstack((color_image, depth_colormap))
 
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', images)
+            cv2.waitKey(1) #delayfor 1ms
 
-finally:
+    finally:
 
-    # Stop streaming
-    pipeline.stop()
+        # Stop streaming
+        pipeline.stop()
--- tts.py
+++ tts.py
@@ -1,19 +1,85 @@
+import multiprocessing,threading, queue
+from multiprocessing import Queue
 import pyttsx3
-import time
 
-def tts_msg(msg, rate, volume) : 
-    engine = pyttsx3.init() # object creation 
-    engine.setProperty('rate', rate)     # setting up new voice rate
-    engine.setProperty('volume', volume)    # setting up volume level between 0 and 1
-    voices = engine.getProperty('voices')       #getting details of current voice
-    engine.setProperty('voice', voices[0].id)   #changing index, changes voices. 1 for female
-    engine.say(msg)
-    engine.runAndWait()
-    engine.stop()
-    engine.save_to_file('Hello World', 'test.mp3')
-    engine.runAndWait()
+# class tts_engine:
+#     def __init__(self) -> None:
+#         self.engine = pyttsx3.init()
+#         if(self.engine == None):
+#             print("enginer is None type ")
+#         self.engine.say("Starting up")
+#         self.engine.runAndWait()
     
+#     def msg(self, message = "Welcome to Banana Vision",rate = 280, volume = 100.0):
+#         self.engine.setProperty('rate', rate)     # setting up new voice rate
+#         self.engine.setProperty('volume', volume)    # setting up volume level between 0 and 1
+#         voices = self.engine.getProperty('voices')       #getting details of current voice
+#         self.engine.setProperty('voice', voices[0].id)   #changing index, changes voices. 1 for female
+#         self.engine.say(message)
 
-start_time = time.time()
-tts_msg("ayo watch out theres a wall", 180, 1.0)
-print("--- %s seconds ---" % (time.time() - start_time))
+
+#     def tts_process(q, tts_engine):
+#         while(True):
+#             msg = str(q.get())
+#             print("in thread" + msg + "\n")
+#             tts_engine.msg(msg)
+
+class TTSProcess(multiprocessing.Process):
+    def __init__(self, queue):
+        multiprocessing.Process.__init__(self)
+        self.queue = queue
+        self.daemon = False
+        self.start()
+    def run(self):
+        tts_engine = pyttsx3.init()
+        tts_engine.setProperty('rate', 280)     # setting up new voice rate
+        tts_engine.setProperty('volume', 100.0)    # setting up volume level between 0 and 1
+        tts_engine.startLoop(False)
+        t_running = True
+        while t_running:
+            if self.queue.empty():
+                tts_engine.iterate()
+            else:
+                data = self.queue.get()
+                if data == "exit":
+                    t_running = False
+                else:
+                    tts_engine.say(data)
+        tts_engine.endLoop()
+
+
+# class TTSThread(threading.Thread):
+#     def __init__(self, queue):
+#         threading.Thread.__init__(self)
+#         self.queue = queue
+#         self.daemon = True
+#         self.start()
+
+#     def run(self):
+#         tts_engine = pyttsx3.init()
+#         tts_engine.setProperty('rate', 280)     # setting up new voice rate
+#         tts_engine.setProperty('volume', 100.0)    # setting up volume level between 0 and 1
+#         tts_engine.startLoop(False)
+#         t_running = True
+#         while t_running:
+#             if self.queue.empty():
+#                 tts_engine.iterate()
+#             else:
+#                 data = self.queue.get()
+#                 if data == "exit":
+#                     t_running = False
+#                 else:
+#                     tts_engine.say(data)
+#         tts_engine.endLoop()
+
+if __name__ == "__main__":
+    # start_time = time.time()
+    # tts_msg("ayo watch out theres a wall", 180, 1.0)
+    # print("--- %s seconds ---" % (time.time() - start_time))
+    q = queue.Queue()
+    tts_thread = TTSThread(q)
+    for item in range(30):
+        print("main : " + str(item) + "\n")
+        q.put(item)
+    q.join()
+    print('All work completed')
